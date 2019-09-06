743cfda06a87da399d8c6c40b264747e411b721e
joseph townshend <joetownshend@mac.com>
2019-09-06 15:38:03 +0100

    setup: properties added to seed and loaded to db

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,41 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Househunter.Repo
+alias Househunter.Listings.Property
+
+%Property{
+  location: "Peckham",
+  type: "Flat",
+  bedrooms: 2
+} |> Repo.insert!
+
+%Property{
+  location: "Hoxton",
+  type: "House",
+  bedrooms: 4
+} |> Repo.insert!
+
+%Property{
+  location: "Mile End",
+  type: "Flat",
+  bedrooms: 1
+} |> Repo.insert!
+
+%Property{
+  location: "Peckham",
+  type: "House",
+  bedrooms: 2
+} |> Repo.insert!
+
+%Property{
+  location: "Mile End",
+  type: "House",
+  bedrooms: 4
+} |> Repo.insert!
+
+%Property{
+  location: "Hoxton",
+  type: "Flat",
+  bedrooms: 1
+} |> Repo.insert!
